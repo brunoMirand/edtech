0205febb7bc0a852d4e57e1f447103c7b15b9582
Bruno Miranda <brunomirandas@outlook.com>
2024-01-17 20:42:10 -0300

    test: create unit tests for use cases

---
+++ test/unit/use-cases/create-content.spec.ts
@@ -0,0 +1,45 @@
+import { InMemoryContentsRepository } from '@/repositories/in-memory/in-memory-contents-repository';
+import { CreateContent } from '@/use-cases/create-content';
+
+let contentRepository: InMemoryContentsRepository;
+let sut: CreateContent;
+type Input = {
+  name: string;
+  description: string;
+  type: 'video' | 'pdf' | 'image';
+};
+
+describe('Use Case - Create Content', () => {
+  beforeEach(() => {
+    contentRepository = new InMemoryContentsRepository();
+    sut = new CreateContent(contentRepository);
+  });
+
+  it('should return success when create content', async () => {
+    //given
+    const inputContent: Input = {
+      name: 'Comunicação Assíncrona',
+      description: 'Aprenda como se comunicar em ambientes remotos',
+      type: 'pdf',
+    };
+    const role = 'admin';
+
+    //when
+    const response = await sut.execute(inputContent, role);
+    //then
+    expect(response.name).toBe('Comunicação Assíncrona');
+  });
+
+  it('should return an exception when trying to create content with a non-admin user', async () => {
+    //given
+    const inputContent: Input = {
+      name: 'Comunicação Assíncrona',
+      description: 'Aprenda como se comunicar em ambientes remotos',
+      type: 'pdf',
+    };
+
+    const role = 'user';
+    //when//then
+    await expect(() => sut.execute(inputContent, role)).rejects.toThrow(new Error('User does not have permission for this feature.'));
+  });
+});
