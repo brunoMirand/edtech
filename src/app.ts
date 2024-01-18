edffe327308c2f14b5bbc993417da3f4931968d6
Bruno Miranda <brunomirandas@outlook.com>
2024-01-18 18:01:37 -0300

    feat: add control over content views per user, solution using cookies

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import fastify from 'fastify';
+import cookie from '@fastify/cookie';
 import { appRoutePublic, appRouteProtected } from '@/http/routes';
 
 export const app = fastify();
 
+app.register(cookie);
 app.register(appRoutePublic);
 app.register(appRouteProtected);
--- src/http/controlles/list-content-by-id.ts
+++ src/http/controlles/list-content-by-id.ts
@@ -1,5 +1,6 @@
 import { FastifyRequest, FastifyReply } from 'fastify';
 import { ContentsUseCasesFactory } from '@/use-cases/factories/contents/make-use-cases';
+import { getContentVisited, setContentVisited } from '@/helpers/cookies-content-visited';
 
 export class ListContentByIdController {
   constructor() { }
@@ -7,11 +8,14 @@ export class ListContentByIdController {
   async handle(request: FastifyRequest, reply: FastifyReply) {
     try {
       const { id } = request.params;
+      const contentViewed = getContentVisited(request);
+
       const listContentById = (ContentsUseCasesFactory.make()).listContentById;
-      const response = await listContentById.execute(id);
+      const response = await listContentById.execute(id, contentViewed);
       if (!response) {
         reply.status(204).send(response);
       }
+      setContentVisited(reply, id, contentViewed);
       reply.status(200).send(response);
     } catch (e) {
       reply.status(400).send(e.message);
