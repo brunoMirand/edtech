e18ee958bdb9ca263efbcd6bbbdac0734f8026cf
Bruno Miranda <brunomirandas@outlook.com>
2024-01-18 14:13:38 -0300

    feat: add new props in factory class

--- src/use-cases/factories/contents/make-use-cases.ts
+++ src/use-cases/factories/contents/make-use-cases.ts
@@ -1,13 +1,19 @@
 import { PrismaContentsRepository } from '@/repositories/prisma/prisma-contents-repository';
 import { CreateContent } from '@/use-cases/create-content';
+import { ListContents } from '@/use-cases/list-contents';
 import { UpdateContent } from '@/use-cases/update-content';
+import { RemoveContent } from '@/use-cases/remove-content';
+import { ListContentById } from '@/use-cases/list-content-by-id';
 
 export class ContentsUseCasesFactory {
   static make() {
     const contentRepository = new PrismaContentsRepository();
     return {
       createContent: new CreateContent(contentRepository),
-      updateContent: new UpdateContent(contentRepository)
+      updateContent: new UpdateContent(contentRepository),
+      listContents: new ListContents(contentRepository),
+      removeContent: new RemoveContent(contentRepository),
+      listContentById: new ListContentById(contentRepository),
     };
   }
 }
