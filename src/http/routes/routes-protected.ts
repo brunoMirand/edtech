81907fa91a6525210b6abef6adb56f61e3e92ef1
Bruno Miranda <brunomirandas@outlook.com>
2024-01-18 14:12:12 -0300

    feat: add new routes

--- src/http/routes/routes-protected.ts
+++ src/http/routes/routes-protected.ts
@@ -2,9 +2,11 @@ import { FastifyInstance } from 'fastify';
 import { CheckPermissionMiddleware } from '@/http/middlewares/check-permission';
 import { CreateContentController } from '@/http/controlles/create-content';
 import { UpdateContentController } from '@/http/controlles/update-content';
+import { RemoveContentController} from '@/http/controlles/remove-content';
 
 export async function appRouteProtected(app: FastifyInstance) {
   app.addHook('preHandler', CheckPermissionMiddleware.handle);
   app.post('/contents', (new CreateContentController).handle);
   app.put('/contents/:id', (new UpdateContentController).handle);
+  app.delete('/contents/:id', (new RemoveContentController).handle);
 }
