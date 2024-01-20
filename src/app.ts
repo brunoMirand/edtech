72b253556214ebef4944932a69383ac2250e9de0
Bruno Miranda <brunomirandas@outlook.com>
2024-01-20 00:57:29 -0300

    style: remove lib not unused

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,4 @@
 import fastify from 'fastify';
-import cookie from '@fastify/cookie';
 import {fastifyJwt} from '@fastify/jwt';
 import { env } from '@/env';
 import { appRoutePublic, appRouteProtected } from '@/http/routes';
@@ -9,6 +8,6 @@ export const app = fastify();
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET_KEY
 });
-app.register(cookie);
+
 app.register(appRoutePublic);
 app.register(appRouteProtected);
