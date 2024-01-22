696d464ee4dca5c235a980d5e9475bbd06de6f62
Bruno Miranda <brunomirandas@outlook.com>
2024-01-22 00:09:52 -0300

    feat: sanitizes and validates query parameters and querystring.

--- src/http/controlles/remove-content.ts
+++ src/http/controlles/remove-content.ts
@@ -1,17 +1,22 @@
 import { FastifyRequest, FastifyReply } from 'fastify';
+import { ZodError } from 'zod';
 import { ContentsUseCasesFactory } from '@/use-cases/factories/contents/make-use-cases';
 import { UnableRemoveContentError } from '@/use-cases/errors/contents-errors';
+import { contentSchemaId } from '@/http/validations/schema';
 
 export class RemoveContentController {
   constructor() { }
 
   async handle(request: FastifyRequest<{ Params: Parameters}>, reply: FastifyReply) {
     try {
-      const { id } = request.params;
+      const id = contentSchemaId.parse(request.params.id);
       const removeContent = (ContentsUseCasesFactory.make()).removeContent;
       await removeContent.execute(id);
       reply.status(204).send();
     } catch (e) {
+      if (e instanceof ZodError) {
+        reply.status(422).send({ message: JSON.parse(e.message) });
+      }
       if (e instanceof UnableRemoveContentError) {
         reply.status(422).send({ message: e.message });
       }
